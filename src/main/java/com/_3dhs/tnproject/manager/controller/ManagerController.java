7dae00f018020b3910d4b8bb4a9a52fb31c3a138
hohojeong091 <hj660833@gmail.com>
2024-03-20 18:23:00 +0900

    #10 관리자 기능 개발 시작

--- src/main/java/com/_3dhs/tnproject/manager/controller/ManagerController.java
+++ src/main/java/com/_3dhs/tnproject/manager/controller/ManagerController.java
@@ -5,8 +5,7 @@ import com._3dhs.tnproject.member.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.Mapping;
-import org.springframework.web.bind.annotation.PostMapping;
+
 
 @Controller
 @RequiredArgsConstructor
