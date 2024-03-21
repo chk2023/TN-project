93a723d5230ca49f30a3619a8af67dfc6fd80cf6
hohojeong091 <hj660833@gmail.com>
2024-03-21 17:38:46 +0900

    #10 3/21 관리자 기능 개발 중

--- src/main/java/com/_3dhs/tnproject/manager/controller/ManagerController.java
+++ src/main/java/com/_3dhs/tnproject/manager/controller/ManagerController.java
@@ -3,10 +3,13 @@ package com._3dhs.tnproject.manager.controller;
 import com._3dhs.tnproject.manager.service.ReportService;
 import com._3dhs.tnproject.member.service.MemberService;
 import lombok.RequiredArgsConstructor;
+import org.apache.logging.log4j.message.Message;
+import org.springframework.context.support.MessageSourceAccessor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.Mapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 
 @Controller
@@ -15,12 +18,24 @@ public class ManagerController {
 
     private final ReportService reportService;
     private final MemberService memberService;
+    private final MessageSourceAccessor messageSourceAccessor;
 
 
 
 
-    @PostMapping("/manage-log")
+    //회원 로그인 페이지가 만들어지면 그쪽으로 경로 변경해주기
+    @GetMapping("/manager/m-log")
     public void loginMag () {
+
+    }
+    //로그인 오류시 나올 페이지랑 연동시키긔.... 경로 경로 경로-_-
+    @GetMapping("/manager/loginfail")
+    public String loginFailed(RedirectAttributes rttr) {
+        rttr.addFlashAttribute("message", messageSourceAccessor.getMessage("error.login"));
+        return "redirect:/manager/manage-log";
+    }
+    @GetMapping ("/manager/m-timeline")
+    public void MagTimeline () {
 
     }
 
