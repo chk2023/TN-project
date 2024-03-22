a67bc3bcab9ef5174fdf19b2d747a052d3c035e3
hohojeong091 <hj660833@gmail.com>
2024-03-22 15:49:52 +0900

    #10 3/22 신고 등록 게시판 만드는 중...

--- database/create_dummyReportDB.sql
+++ database/create_dummyReportDB.sql
@@ -1,28 +1,6 @@
 
-
-
-INSERT INTO tbl_report(r_category_code)
-VALUES ('1'),
-       ('2'),
-       ('3'),
-       ('4');
-
-INSERT INTO tbl_report(report_text)
-VALUES ('너무 심한 모욕감을 주는 글을 씀');
-
-INSERT INTO tbl_report(report_date)
-VALUES ('24/03/22');
-
-
-INSERT INTO tbl_report(report_status)
-VALUES ('신고 내역 확인 중..');
-
-INSERT INTO tbl_report(processing_date)
-VALUES ('24/03/22');
-
-INSERT INTO tbl_report(processing_text)
-VALUES ('해당 글을 확인해보니, 모욕감이 느껴짐.');
-
+INSERT INTO tbl_report(r_category_code, report_text, report_date, report_status, processing_date, processing_text, sub_member_code, manager_code, reporter_code, report_content, post_code, cmt_code)
+VALUES ('1', '혐오 발언', Localtime, '신고 횟수 수정', LOCALTIME, '계정 정지', '3', '1', '4', '게시글','1' , null  );
 
 
 
