866d26b3f40f58aef02873100ab2896780181042
MIdWit0828 <kindj9897@gmail.com>
2024-03-22 18:24:25 +0900

    #30 최초커밋

--- src/main/java/com/_3dhs/tnproject/post/controller/PostController.java
+++ src/main/java/com/_3dhs/tnproject/post/controller/PostController.java
@@ -3,9 +3,15 @@ package com._3dhs.tnproject.post.controller;
 import com._3dhs.tnproject.post.service.PostService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 @RequiredArgsConstructor
 public class PostController {
     private final PostService postService;
+
+    @GetMapping("post/likelist")
+    public void findLikeListByMemberCode(int memberCode) {
+
+    }
 }
