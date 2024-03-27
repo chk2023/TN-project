4645c2a14877d9c88dff806cc3d8d70ff1a0f9fb
MIdWit0828 <kindj9897@gmail.com>
2024-03-27 16:58:47 +0900

    #20 무한스크롤 구현 완료

--- src/main/resources/static/js/timeline.js
+++ src/main/resources/static/js/timeline.js
@@ -1,11 +1,14 @@
 const $blogList = document.querySelector(".blogList");
-update(); //최초 1회 실행
+const $main = document.querySelector("main");
+
+// update(); //최초 1회 실행
 function update() {
     fetch(`/timeline/updateList?index=${index}&range=${range}&contentsType=${contentsType}`)
         .then(res => res.json())
         .then(data => {
+            console.log("업데이트 실행됨")
             if (!data.length) {
-                alert("더이상 보여줄 글이 없습니다.");
+                alert("이런! 오늘은 글이 더 없네요.");
                 return;
             }
             // 템플릿 소스 설정
@@ -23,6 +26,11 @@ function update() {
                 item.cmtCount = formatCount(item.cmtCount);
                 item.likeCount = formatCount(item.likeCount);
                 item.postWriDate = formatWriDate(item.postWriDate);
+                if (!isTitlePhotoChanged && item.thumbnailPath !== `/image/icon_default_photo.png`) {
+                    //이미지가 있는 가장 첫번째 사진으로 타이틀사진을 변경
+                    setTitlePhoto(item.thumbnailPath);
+                    isTitlePhotoChanged = true;
+                }
             });
             var html = document.createElement("li");
             html.innerHTML = template(data, {data: {handlebars: Handlebars.create()}});
@@ -31,7 +39,15 @@ function update() {
             $blogList.appendChild(fragment);
 
             index = index + range;
-        }).catch(error => console.error('Error fetching profile data:', error));
+        }).catch(error => console.error('Error fetching profile data:', error))
+        .finally(function () {
+            isLoading = false
+        });
+}
+
+function setTitlePhoto(path) {
+    $img = document.querySelector(".mainProfile img");
+    $img.src = path;
 }
 
 function formatWriDate(postWriDate) {
@@ -50,22 +66,37 @@ function formatCount(count) {
     }
 }
 
-function latestBtnClicked() {
-    contentsType = 2;
-    btnProcess();
-    $tabMenu.querySelector("#latest").classList.add("active");
-}
-
 function trendBtnClicked() {
     contentsType = 1;
     btnProcess();
     $tabMenu.querySelector("#trend").classList.add("active");
 }
 
+function latestBtnClicked() {
+    contentsType = 2;
+    btnProcess();
+    $tabMenu.querySelector("#latest").classList.add("active");
+}
+
 function btnProcess() {
     $blogList.innerHTML = "";
     index = 0;
+    isTitlePhotoChanged = false;
     update();
-    childList = $blogList.querySelectorAll("*");
-    childList.forEach(element => element.classList.remove("active"))
+    childList = $tabMenu.querySelectorAll("*");
+    console.log(childList);
+    childList.forEach(element => element.classList.remove("active"));
 }
+//무한 스크롤 이벤트
+$main.addEventListener("scroll", function () {
+    let scrollTop = $main.scrollTop;
+    let viewportHeight = $main.clientHeight;
+    let containerHeight = $main.scrollHeight;
+    console.log("scrollTop : " + scrollTop);
+    console.log("vHeight : " + viewportHeight);
+    console.log("cHeight :" + containerHeight);
+    if (scrollTop + viewportHeight >= containerHeight && !isLoading) {
+        isLoading = true;
+        update();
+    }
+});
