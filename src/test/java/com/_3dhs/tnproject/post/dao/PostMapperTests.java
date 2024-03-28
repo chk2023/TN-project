4b52b256f99d4061b6986817e9ceffee4a15f705
MIdWit0828 <kindj9897@gmail.com>
2024-03-28 15:46:44 +0900

    #46 03/28 테스트 컨트롤러, 서비스, 매퍼에 대한 테스트 결과 : 성공

--- src/test/java/com/_3dhs/tnproject/post/dao/PostMapperTests.java
+++ src/test/java/com/_3dhs/tnproject/post/dao/PostMapperTests.java
@@ -1,6 +1,7 @@
 package com._3dhs.tnproject.post.dao;
 
 import com._3dhs.tnproject.post.dto.LikeListDTO;
+import com._3dhs.tnproject.post.dto.PostDTO;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -13,4 +14,13 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 public class PostMapperTests {
     @Autowired
     private PostMapper mapper;
+
+    @Test
+    void findLikeListPostByMemberCode_테스트() {
+        int rdCode = (int)(Math.random() * 12) + 1;
+        List<PostDTO> testList = mapper.findLikeListPostByMemberCode(rdCode);
+        System.out.println("memberCode : " + rdCode);
+        testList.forEach(System.out::println);
+        assertNotNull(testList);
+    }
 }
