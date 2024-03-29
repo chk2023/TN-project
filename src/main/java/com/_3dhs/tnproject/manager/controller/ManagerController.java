77a7180d2a722396e038b303aff1fde3a2799f86
hohojeong091 <hj660833@gmail.com>
2024-03-29 10:20:29 +0900

    #53 관리 기록 조회 페이지 연동 만드는 중..

--- src/main/java/com/_3dhs/tnproject/manager/controller/ManagerController.java
+++ src/main/java/com/_3dhs/tnproject/manager/controller/ManagerController.java
@@ -2,11 +2,13 @@ package com._3dhs.tnproject.manager.controller;
 
 import com._3dhs.tnproject.manager.dto.ReportDTO;
 import com._3dhs.tnproject.manager.service.ReportService;
+import com._3dhs.tnproject.member.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.support.MessageSourceAccessor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
@@ -14,29 +16,24 @@ import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
-public  class ManagerController {
+public class ManagerController {
 
     private final ReportService reportService;
+    private final MemberService memberService;
     private final MessageSourceAccessor messageSourceAccessor;
 
 
-    //회원 로그인 페이지가 만들어지면 그쪽으로 경로 변경해주기
-    @GetMapping("/manager/m-log")
-    public void loginMag() {
+    @GetMapping("/manager/reportList")
+    public String viewAllReportList(Model model) {
+
+        List<ReportDTO> reportList = reportService.viewAllReport();
+        model.addAttribute("reportList", reportList);
+
+        return "manager/reportList";
+
 
     }
 
-    //로그인 오류시 나올 페이지랑 연동시키긔.... 경로 경로 경로-_-
-    @GetMapping("/manager/loginfail")
-    public String loginFailed(RedirectAttributes rttr) {
-        rttr.addFlashAttribute("message", messageSourceAccessor.getMessage("error.login"));
-        return "redirect:/manager/manage-log";
-    }
-
-    @GetMapping("/manager/m-timeline")
-    public void MagTimeline() {
-
-    }
 
     @GetMapping("/manager/report/list")
     public void viewAllReportList(Model model) {
@@ -52,8 +49,8 @@ public  class ManagerController {
         model.addAttribute("report", report);
 
 
-        return"/manager/report/detail";
-}
+        return "/manager/report/detail";
+    }
 
     @GetMapping("/detail")
     public String updateReport(ReportDTO reportDTO, RedirectAttributes rttr) {
@@ -77,22 +74,14 @@ public  class ManagerController {
         //서브밋이 결과에 따라 알럿창을 띄워주는 기능을 개발해야함.
 
         rttr.addFlashAttribute("insertRecord");
-        return"redirect:/manager/report/list";
+        return "redirect:/manager/report/list";
 
 
     }
 
-
-
-
-
-
-
-
-
+    @GetMapping("/manager/admin/list")
+    public String showAdminList (ReportDTO reportDTO) {
+        ReportDTO admin = reportService.showAdminList(reportDTO);
+                return "/manager/admin/adminList";
+    }
 }
-
-
-
-
-
