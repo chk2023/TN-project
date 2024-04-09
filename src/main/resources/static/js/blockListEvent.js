b2b3edddf975a119e6863847718912b17cbe819b
MIdWit0828 <kindj9897@gmail.com>
2024-04-09 16:45:58 +0900

    #100 수정 - 글코드를 받아서도 작동하도록 Controller추가 - blockListEvent의 메소드를 통해서 차단할 유저를 정할수 있게 설정

--- src/main/resources/static/js/blockListEvent.js
+++ src/main/resources/static/js/blockListEvent.js
@@ -1,3 +1,29 @@
 function unblockMemberByCode(code) {
     window.location.href = "/member/unblockMember?targetMemberCode=" + code;
 }
+
+function blockMemberByCode(code, contentsType) {
+    //컨트롤러에서는 기본값( 0 )인 변수는 처리 하지않도록 구성함
+    let postCode = new Number(0);
+    let cmtCode = new Number(0);
+    //콘텐츠 타입에 따라 postCode 혹은 cmtCode에 값을 넣어줌
+    if (contentsType === "post") {
+        postCode = code;
+    } else if (contentsType === "cmt") {
+        cmtCode = code;
+    }
+    fetch("/member/blockMember", {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json'
+        }, body: JSON.stringify({postCode: postCode, cmtCode: cmtCode})
+    }).then(response => {
+        if (!response.ok) {
+            throw new Error("응답오류");
+        }
+        return response.text();
+    }).then(data => {
+        // 반환하는 값은 message이므로 alert로 출력
+        alert(data);
+    });
+}
