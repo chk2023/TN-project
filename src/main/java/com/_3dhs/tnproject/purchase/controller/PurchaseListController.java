f0191c98497dac8ce5dc9c786b2420182594b0a5
syyyl <lxxsxyxx@gmail.com>
2024-04-11 04:42:53 +0900

    #116 구매 내역 기능 작성

---
+++ src/main/java/com/_3dhs/tnproject/purchase/controller/PurchaseListController.java
@@ -0,0 +1,34 @@
+package com._3dhs.tnproject.purchase.controller;
+
+
+import com._3dhs.tnproject.member.dto.MemberDTO;
+import com._3dhs.tnproject.purchase.dto.PurchaseDTO;
+import com._3dhs.tnproject.purchase.service.PurchaseService;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@Controller
+public class PurchaseListController {
+
+    private final PurchaseService purchaseService;
+
+    public PurchaseListController(PurchaseService purchaseService) {
+        this.purchaseService = purchaseService;
+    }
+
+
+    @GetMapping("/purchase/list")
+    public String getPurchaseListPage (Model model, @AuthenticationPrincipal MemberDTO memberDTO) {
+
+        List<PurchaseDTO> purchaseList = purchaseService.getPurchaseList(memberDTO.getMemberCode());
+        model.addAttribute("purchaseList", purchaseList);
+
+        return "/purchase/list";
+    }
+
+
+}
