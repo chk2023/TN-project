0ed1f27d1f79b22220c7ed2fd76338465ad78a5c
MIdWit0828 <kindj9897@gmail.com>
2024-04-16 16:19:56 +0900

    #153 프로젝트 종료 후 리펙토링 PostController 에서 좋아요 관련 기능 분리 LikeService 패키지 변경 timeline.js 타임라인에서 콘텐츠 업로드 시 div에 담겨 출력되는 현상 fix ------------(04/16) blogMain.js 에서 옵션버튼 이벤트 분리 main.html에 optionBtnEvent.js추가

--- src/main/resources/static/js/blogMain.js
+++ src/main/resources/static/js/blogMain.js
@@ -26,32 +26,6 @@ function tabMenuClick(e) {
     }
 }
 
-document.addEventListener('click', function (e) {
-    const $optionBtn = document.querySelectorAll(".optionBtn"); //모든 .optionBtn 요소를 선택
-    const targetOptionBtn = e.target.closest(".optionBtn"); //클릭된 요소 또는 그 조상 중 .optionBtn을 찾음
-    const targetViewTypeBtn = e.target.closest(".viewBtn"); //클릭된 요소 또는 그 조상 중 .viewBtn을 찾음
-    const $viewBtn = document.querySelector('.viewBtn > button'); //.viewBtn 눌렀을때 active 걸릴 버튼
-
-    if (targetOptionBtn) {
-        $optionBtn.forEach((option) => {
-            option.classList.remove("active"); //먼저 모든 .optionBtn에서 active 클래스 제거
-        });
-        targetOptionBtn.classList.add("active"); //클릭된 .optionBtn에만 active 클래스 추가
-    } else if (targetViewTypeBtn) {
-        $viewBtn.classList.toggle("active"); //클릭된 .viewBtn 바로 아래 버튼에 토글 active 클래스
-        if (e.target.textContent === "블로그식 보기") {
-            $blogList.classList.remove("snsList"); //블로그식 보기 클릭시 snsList 클래스 제거
-        } else if (e.target.textContent === "SNS식 보기") {
-            $blogList.classList.add("snsList"); //sns식 보기 클릭시 snsList 클래스 추가
-        }
-    } else {
-        $optionBtn.forEach((option) => {
-            option.classList.remove("active"); //모든 .optionBtn에서 active 클래스 제거
-        });
-        $viewBtn.classList.remove("active"); //.viewBtn 바로 아래 버튼 active 클래스 제거
-    }
-});
-
 function formatCount(count) {
     if (count > 1000) {
         return (count / 1000).toFixed(1) + 'k';
--- src/main/resources/static/js/optionBtnEvent.js
+++ src/main/resources/static/js/optionBtnEvent.js
@@ -1,23 +1,25 @@
 document.addEventListener('click', function (e) {
     const $optionBtn = document.querySelectorAll(".optionBtn"); //모든 .optionBtn 요소를 선택
     const targetOptionBtn = e.target.closest(".optionBtn"); //클릭된 요소 또는 그 조상 중 .optionBtn을 찾음
-    const targetViewBtn = e.target.closest(".viewBtn");
+    const targetViewTypeBtn = e.target.closest(".viewBtn"); //클릭된 요소 또는 그 조상 중 .viewBtn을 찾음
+    const $viewBtn = document.querySelector('.viewBtn > button'); //.viewBtn 눌렀을때 active 걸릴 버튼
+
     if (targetOptionBtn) {
         $optionBtn.forEach((option) => {
             option.classList.remove("active"); //먼저 모든 .optionBtn에서 active 클래스 제거
         });
         targetOptionBtn.classList.add("active"); //클릭된 .optionBtn에만 active 클래스 추가
-    }else if (targetViewBtn) {
-        $viewBtn.forEach((option) => {
-            option.classList.remove("active");
-        });
-        $viewBtn.classList.add("active");
+    } else if (targetViewTypeBtn) {
+        $viewBtn.classList.toggle("active"); //클릭된 .viewBtn 바로 아래 버튼에 토글 active 클래스
+        if (e.target.textContent === "블로그식 보기") {
+            $blogList.classList.remove("snsList"); //블로그식 보기 클릭시 snsList 클래스 제거
+        } else if (e.target.textContent === "SNS식 보기") {
+            $blogList.classList.add("snsList"); //sns식 보기 클릭시 snsList 클래스 추가
+        }
     } else {
         $optionBtn.forEach((option) => {
             option.classList.remove("active"); //모든 .optionBtn에서 active 클래스 제거
         });
-        if (targetViewBtn) {
-            $viewBtn.classList.remove("active"); //.viewBtn 바로 아래 버튼 active 클래스 제거
-        }
+        $viewBtn.classList.remove("active"); //.viewBtn 바로 아래 버튼 active 클래스 제거
     }
 });
